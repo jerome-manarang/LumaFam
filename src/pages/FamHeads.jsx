1d1e03ba461b9ee8bd9e9f5ee68dc50c4840d9fc
Harrison Tran <harrit4@uci.edu>
2025-04-03 15:49:39 -0700

    Implemented a carousel of fam heads

--- src/pages/FamHeads.jsx
+++ src/pages/FamHeads.jsx
@@ -1,7 +1,7 @@
 // DIFFICULTY: Medium
 
-// Developer: [Your Name]
-// Date Start: [MM/DD/YYYY]
+// Developer: Harrison Tran
+// Date Start: 03/27/25
 // Date End: [MM/DD/YYYY]
 
 /*
@@ -16,12 +16,42 @@
                           DESCRIPTION
 
 */
+import { useState } from "react";
+import { Carousel } from "flowbite-react";
+import cayz_pfp from "../assets/FamHeads/cayz_pfp.png"
+import hailey_pfp from "../assets/FamHeads/hailey_pfp.png"
+import janice_pfp from "../assets/FamHeads/janice_pfp.png"
+import jerome_pfp from "../assets/FamHeads/jerome_pfp.png"
 
 export default function FamHeads() {
+    var subtitle_array = {}
+    subtitle_array[0] = "Cayz Paclibar";
+    subtitle_array[1] = "Hailey Guman";
+    subtitle_array[2] = "Janice Ho";
+    subtitle_array[3] = "Jerome Manarang";
+
+    var desc_array = {}
+    desc_array[0] = "Cayz Description";
+    desc_array[1] = "Hailey Description";
+    desc_array[2] = "Janice Description";
+    desc_array[3] = "Jerome Description";
+
+    const [currentIndex, setCurrentIndex] = useState(0);
     return (
-      <div className="text-3xl font-bold text-yellow-400 text-center my-4">
-        
-      </div>
+      <div className="h-56 w-56 m-auto text-yellow-400 font-bold">
+            <div className="text-2xl">
+              Fam Heads
+            </div>
+            <Carousel onSlideChange={(index) => setCurrentIndex(index)} className="mt-6">
+              <img src={cayz_pfp} alt="..." />
+              <img src={hailey_pfp} alt="..." />
+              <img src={janice_pfp} alt="..." />
+              <img src={jerome_pfp} alt="..." />
+            </Carousel>
+            
+            <div id="subtitle" className="mt-6">{subtitle_array[currentIndex]}</div>
+            <div id="description">{desc_array[currentIndex]}</div>
+          </div>
     );
   }
   
