917aa0c7adfb95cc40845414e797beb383e42c45
Jerome Manarang <jemanara@uci.edu>
2025-04-07 11:15:01 -0700

    prev

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import './App.css'
 
 export default function App() {
   return (
-    <div className="text-3xl font-bold text-yellow-400">
-      Hello Luma Fam! ✨
+    <div className="background-stars">
+      
     </div>
   );
 }
