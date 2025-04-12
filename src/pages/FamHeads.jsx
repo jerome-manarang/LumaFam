9d420403dba588d8e4f191991efff2f4e0d266ed
Farin Soriano <72371352+Niraaf@users.noreply.github.com>
2025-04-12 00:41:22 -0700

    made fam heads section better, fixed starry background

--- src/pages/FamHeads.jsx
+++ src/pages/FamHeads.jsx
@@ -1,12 +1,10 @@
 // DIFFICULTY: Medium
-
 // Developer: Harrison Tran
 // Date Start: 03/27/25
 // Date End: [MM/DD/YYYY]
 
 /*
- TODO: Create the famheads section for the Luma Fam website.
-git 
+ TODO: Create the FamHeads section for the Luma Fam website.
  Please put images in the assets folder for organization.
  The title is Fam Heads, and there should be an image and text below it.
  Each item in the carousel has this format:
@@ -14,44 +12,60 @@ git
                             IMAGE
                            SUBTITLE
                           DESCRIPTION
-
 */
+
 import { useState } from "react";
 import { Carousel } from "flowbite-react";
-import cayz_pfp from "../assets/FamHeads/cayz_pfp.png"
-import hailey_pfp from "../assets/FamHeads/hailey_pfp.png"
-import janice_pfp from "../assets/FamHeads/janice_pfp.png"
-import jerome_pfp from "../assets/FamHeads/jerome_pfp.png"
+
+import cayz_pfp from "../assets/FamHeads/cayz_pfp.png";
+import hailey_pfp from "../assets/FamHeads/hailey_pfp.png";
+import janice_pfp from "../assets/FamHeads/janice_pfp.png";
+import jerome_pfp from "../assets/FamHeads/jerome_pfp.png";
+
+// Update descriptions later ! and use less pixely images
+const famHeadsData = [
+  {
+    image: cayz_pfp,
+    subtitle: "Cayz Paclibar",
+    description: "Cayz Description",
+  },
+  {
+    image: hailey_pfp,
+    subtitle: "Hailey Guman",
+    description: "Hailey Description",
+  },
+  {
+    image: janice_pfp,
+    subtitle: "Janice Ho",
+    description: "Janice Description",
+  },
+  {
+    image: jerome_pfp,
+    subtitle: "Jerome Manarang",
+    description: "Jerome Description",
+  },
+];
 
 export default function FamHeads() {
-    var subtitle_array = {}
-    subtitle_array[0] = "Cayz Paclibar";
-    subtitle_array[1] = "Hailey Guman";
-    subtitle_array[2] = "Janice Ho";
-    subtitle_array[3] = "Jerome Manarang";
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    var desc_array = {}
-    desc_array[0] = "Cayz Description";
-    desc_array[1] = "Hailey Description";
-    desc_array[2] = "Janice Description";
-    desc_array[3] = "Jerome Description";
+  const currentItem = famHeadsData[currentIndex];
 
-    const [currentIndex, setCurrentIndex] = useState(0);
-    return (
-      <div className="h-56 w-56 m-auto text-yellow-400 font-bold">
-            <div className="text-2xl">
-              Fam Heads
-            </div>
-            <Carousel onSlideChange={(index) => setCurrentIndex(index)} className="mt-6">
-              <img src={cayz_pfp} alt="..." />
-              <img src={hailey_pfp} alt="..." />
-              <img src={janice_pfp} alt="..." />
-              <img src={jerome_pfp} alt="..." />
-            </Carousel>
-            
-            <div id="subtitle" className="mt-6">{subtitle_array[currentIndex]}</div>
-            <div id="description">{desc_array[currentIndex]}</div>
-          </div>
-    );
-  }
-  
+  return (
+    <div className="flex flex-col min-h-screen w-full m-auto text-yellow-400 font-bold items-center justify-center">
+      <div className="text-2xl mt-4">Meet your Fam Heads!</div>
+
+      <Carousel
+        onSlideChange={(index) => setCurrentIndex(index)}
+        className="h-100 w-100 mt-6 "
+      >
+        {famHeadsData.map((item, index) => (
+          <img key={index} src={item.image} alt={item.subtitle} />
+        ))}
+      </Carousel>
+
+      <div className="mt-6 text-lg">{currentItem.subtitle}</div>
+      <div className="text-sm text-yellow-300 font-normal">{currentItem.description}</div>
+    </div>
+  );
+}
